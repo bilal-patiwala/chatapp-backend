82ad11f9bae68db5b75052cd2c3c57da5328b9e9
bilal Patiwala <bilalpatiwala04@gmail.com>
2023-05-29 11:11:48 +0530

    update

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -197,8 +197,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -209,4 +209,5 @@ CORS_ORIGIN_WHITELIST = (
   'http://localhost:3000',
   'http://127.0.0.1:3000',
   'http://localhost:8000',  
+  'https://chatapp-backend-seven.vercel.app/'
 )
